1bf98c561bc27ce8be58bb57913ad39232c04885
portfolio-demo <nehaagarwal1900@gmail.com>
2025-04-02 15:55:51 +0530

    project completed

---
+++ script.js
@@ -0,0 +1,14 @@
+let menuBar = document.getElementById("menuBar");
+menuBar.style.maxHeight = "0px";
+function toggle() {
+    if (menuBar.style.maxHeight == "0px") {
+        menuBar.style.maxHeight = "800px"
+    } else {
+        menuBar.style.maxHeight = "0px"
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".skill-fill").forEach(bar => {
+        bar.style.width = bar.style.width;
+    });
+});
